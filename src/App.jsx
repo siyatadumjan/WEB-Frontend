41b382a845fe01f24662e8619bb4c883113e03c3
siyatadumjan <135720118+siyatadumjan@users.noreply.github.com>
2025-01-05 22:36:17 +0545

    added settings

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const {authUser,checkAuth,isCheckingAuth}= useAuthStore();
+  const {theme}=useThemeStore()
 
   useEffect(() => {
     checkAuth();
@@ -29,7 +31,7 @@ const App = () => {
   );
 
   return (
-  <div data-theme="retro">
+  <div data-theme={theme}>
     <Navbar />
 
     <Routes>
